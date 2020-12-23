616589ac4142be08a121bfdfb7d5c58f5134d0d8
Michael Cummings <dragonrun1@gmail.com>
2020-12-23 06:55:21 -0800

    :memo: Fixed MIT license text in code. [ci skip]

--- src/error.rs
+++ src/error.rs
@@ -23,8 +23,8 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -94,10 +94,4 @@ impl From<U64Error> for std::str::Utf8Error {
     fn from(ue: U64Error) -> Self {
         ue.into()
     }
-}
-
-#[cfg(test)]
-mod tests {
-    #[allow(unused_imports)]
-    use super::*;
 }
